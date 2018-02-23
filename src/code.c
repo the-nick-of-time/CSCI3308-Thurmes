271afd162ac5e7b19f3c27dc656096e9d3cf7ff8
Nicholas Thurmes <nith2593@colorado.edu>
2018-02-23 09:26:40 -0700

    technically this code should run

--- src/code.c
+++ src/code.c
@@ -2,6 +2,18 @@
 
 int main(int argc, char** argv)
 {
-	printf("called with %d arguments", argc);
+	printf("called with %d arguments\n", argc);
+	printf("hash of first argument: %d\n", hash(argv[0]));
 	return 0;
+}
+
+
+
+int hash(char* str)
+{
+	int h;
+	for (char* p=str; *p; p++) {
+		h += p;
+	}
+	return h;
 }
